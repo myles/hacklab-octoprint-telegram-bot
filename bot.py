d69397b86ad2d8d3ca37f6a6ca692d3f3f08400c
Myles Braithwaite <me@mylesbraithwaite.com>
2016-08-17 14:13:07 -0400

    Fixed up some stuff.

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-import sys
 import json
 import logging
 
@@ -6,7 +5,6 @@ from telegram.ext import Updater, CommandHandler
 
 from octoprint import OctoPrint
 
-sys.path.append('./future-0.15.2/src/')
 
 logging.basicConfig(filename='bot.log', format='%(asctime)s %(message)s',
                     level=logging.DEBUG)
